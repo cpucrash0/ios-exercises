43fadda37d25cf2f20aa9d71293d9ea5938d875f
cpucrash0 <cpucrash0@gmail.com>
2016-06-16 17:42:13 -0500

    Objective-C: If, Else, and the Ternary Operator

---
+++ MarysAppleHandler.m
@@ -0,0 +1,57 @@
+//
+//  MarysAppleHandler.m
+//  BlocExercises
+//
+//  Created by Aaron on 6/8/14.
+//
+//
+
+#import "MarysAppleHandler.h"
+
+@implementation MarysAppleHandler
+
+- (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
+    NSString *itemToReturn;
+    if (dollars<5) {
+        itemToReturn = @"get out of my store";
+    }else if (dollars==5) {
+        itemToReturn = @"have some gum";
+    } else if (dollars> 5 && dollars<1000)  {
+        itemToReturn = @"have an apple";
+        
+    } else if (dollars>=1000 && dollars<1000000000)  {
+        itemToReturn = @"have an Apple computer";
+        
+    } else  {
+        itemToReturn = @"have The Big Apple";
+    }
+    
+    /* WORK HERE */
+    
+    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+    return itemToReturn;
+}
+
+- (NSUInteger) dollarCostForAppleFlavoredVodka  {
+    
+    NSUInteger cost = 24;
+    
+    cost = self.getsDiscount == YES ? cost * .75 : cost ;
+    
+    return cost;
+}
+
+/*
+ BOOL isTrue = YES;
+ 
+ if (isTrue) {
+ NSLog(@"do Something");
+ } else {
+ NSLog(@"somethingElse");
+ }
+ 
+ NSLog(isTrue ? @"do Something" : @"somethingElse");
+ */
+
+@end
+
